2a470cc593926bb91eb76c9397afe70ca2e02abd
Kaveer <41718548+iamkaveer@users.noreply.github.com>
2023-03-12 23:59:31 +0530

    Delete deployment directory

--- deployment/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aws.deployment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.aws.deployment.model.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Integer> {
-
-}
